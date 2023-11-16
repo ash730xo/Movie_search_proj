52639bc7334d07c75846b5d691faf69a4bcdb895
ash730xo <ash730@gmail.com>
2023-11-16 14:43:08 -0500

    movie card with API data showing multiple movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React from "react"
-import { useEffect } from "react"
+import React, { useState , useEffect} from "react"
 
 import './App.css'
 import searchIcon from './search.svg'
@@ -9,6 +8,7 @@ import MovieCard from "./MovieCard"
 
 export default function App() {
 const API_URL = "http://www.omdbapi.com/?apikey=4350f241 "
+const [movies, setMovies] = useState([])
 
 
 const movie1 = {
@@ -24,7 +24,7 @@ const searchMovies = async (title) => {
   const response = await fetch(`${API_URL}&s=${title}`)
   const data = await response.json()
 
-  console.log(data.Search)
+  setMovies(data.Search)
 
 }
 
@@ -48,9 +48,24 @@ useEffect(() => {
         />
       </div>
 
-      <div className="container">
-        <MovieCard movie1={movie1}/>
-      </div>
+      {
+        movies?.length > 0 
+        ? (
+          <div className="container">
+            {
+              movies.map((movie) => (
+                <MovieCard movie={movie}/>
+              ))
+            }
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )
+      }
+
+
     </div>
   )
 }
