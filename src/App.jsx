794106e4fd9a6e1f4f16b615a830c3601cd9b542
ash730xo <ash730@gmail.com>
2023-11-16 14:09:01 -0500

    Movie API, useEffect console.log search

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react"
-
+import { useEffect } from "react"
+// http://www.omdbapi.com/?i=tt3896198&apikey=4350f241 
+//4350f241
 export default function App() {
-  
+const API_URL = "http://www.omdbapi.com/?apikey=4350f241 "
 
+//function for use effect
+const searchMovies = async (title) => {
+  const response = await fetch(`${API_URL}&s=${title}`)
+  const data = await response.json()
+
+  console.log(data.Search)
+
+}
+
+useEffect(() => {
+  searchMovies('SpiderMan')
+},[])
   return (
     <>
-      <h1>Test</h1>
+      <h1>App</h1>
     </>
   )
 }
