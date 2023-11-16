ced10cdc16221d7ed71f5f9289df52a67ab544de
ash730xo <ash730@gmail.com>
2023-11-16 14:24:46 -0500

    finsihed movie card w/static data

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,11 @@ useEffect(() => {
           <div>
             <img src={movie1.Poster !== 'N/A' ? movie1.Poster : "http://via.placeholder.com/400"} alt={movie1.Title}/>
           </div>
+
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
         </div>
       </div>
     </div>
