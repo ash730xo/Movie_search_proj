17cbb9d45be681e4c84ce4139e73b87df60ba5a7
ash730xo <ash730@gmail.com>
2023-11-16 14:57:48 -0500

    app.jsx comments

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ const [movies, setMovies] = useState([])
 const [searchTerm, setSearchTerm] = useState("")
 
 //function for use effect
+//async function 
+//whatever title is inputed it waits for the response
 const searchMovies = async (title) => {
   const response = await fetch(`${API_URL}&s=${title}`)
   const data = await response.json()
@@ -20,25 +22,49 @@ const searchMovies = async (title) => {
 
 }
 
+//useEffect 
 useEffect(() => {
-  searchMovies('SpiderMan')
+    //what renders during first load since the dependency array is empty
+  searchMovies('Hello Kitty')
 },[])
   return (
 
+    // App name 
     <div className="app">
       <h1>MovieLand</h1>
       <div className="search">
+
+
+      {/* 
+        Search movie input
+        sets the value for whatever the user is typing by using the searchTerm useState
+      */}
         <input 
           placeholder="Search for movies"
           value={searchTerm}
+          // takes the input on change 
           onChange={(e) => setSearchTerm(e.target.value)}
         />
+
+        {/* 
+          Search image
+          Whenever it is clicked it updated the search to the movie desired
+         */}
         <img 
+          // displays image that is imported 
           src={searchIcon}
           alt="search"
+          // when image is click set the search to the user input
           onClick={() => searchMovies(searchTerm)}
         />
       </div>
+
+      {/* 
+         lines 70 - 85
+         Turnery function 
+         if the movie length is more than 0 display the movie cards (whatever it was hard coded to (line 28 in the useEffect function))
+         If the lenght is not more than 0 then display np moves found
+       */}
 
       {
         movies?.length > 0 
@@ -46,6 +72,7 @@ useEffect(() => {
           <div className="container">
             {
               movies.map((movie) => (
+                /* Displays the movie card component while setting the prop */
                 <MovieCard movie={movie}/>
               ))
             }
